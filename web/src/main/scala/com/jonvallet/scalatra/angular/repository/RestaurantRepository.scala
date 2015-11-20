e9a2d17adcf8153c775a65d0e75f071fc3835e80
Jon Vallet <j.vallet@gmail.com>
2015-11-20 11:30:51 +0000

    Add get all restaurants

--- web/src/main/scala/com/jonvallet/scalatra/angular/repository/RestaurantRepository.scala
+++ web/src/main/scala/com/jonvallet/scalatra/angular/repository/RestaurantRepository.scala
@@ -2,6 +2,8 @@ package com.jonvallet.scalatra.angular.repository
 
 import com.jonvallet.scalatra.angular.database.DatabaseContext
 import com.jonvallet.scalatra.angular.database.generated.tables.Restaurant._
+import com.jonvallet.scalatra.angular.database.generated.tables.records.RestaurantRecord
+import org.json4s.jackson.Json
 import scala.collection.JavaConversions._
 import org.jooq.scala.Conversions._
 
@@ -9,14 +11,12 @@ import org.jooq.scala.Conversions._
  * Created by jvallet on 20/11/15.
  */
 class RestaurantRepository(ctx: DatabaseContext) {
-  def list(): List[Restaurant] = {
+  def list() = {
     ctx.create
        .select()
        .from(RESTAURANT)
-       .fetchInto(classOf[Restaurant])
-       .toList
+       .fetch()
+       .formatJSON()
   }
 
 }
-
-case class Restaurant(id: Int, name: String, description: String)
