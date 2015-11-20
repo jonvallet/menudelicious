d8a1c4c6bef79ddf3a4fbdeed5c7d1b4f9fde3c5
VictoriaJames <victoriaamps@gmail.com>
2015-11-20 20:12:47 +0000

    style improvements

--- web/src/main/webapp/js/restaurant_cont.js
+++ web/src/main/webapp/js/restaurant_cont.js
@@ -33,30 +33,46 @@ restaurantApp.controller('RestaurantCtrl', function ($scope,$http) {
 
   $scope.chooseRestaurant = function(restaurant_id) {
 
+  for (index = 0; index < $scope.restaurants.length; ++index) {
+    if ($scope.restaurants[index][0] == restaurant_id) {
+        $scope.chosenRestaurant = {
+            "name": $scope.restaurants[index][1],
+            "description": $scope.restaurants[index][2],
+            "address": $scope.restaurants[index][3] + " " +
+            $scope.restaurants[index][4] + " " +
+            $scope.restaurants[index][5] + " " +
+            $scope.restaurants[index][6] + " " +
+            $scope.restaurants[index][7] + " "
+        };
+    }
+    }
+
     $http.get('/api/restaurant/'+ restaurant_id +'/menu').then(function successCallback(response) {
 
         var menu_items1 = response.data.records;
 
-
-
         for (index = 0; index < menu_items1.length; ++index) {
 
         var item = {
-                                "id": menu_items1[index][0],
-                                "name": menu_items1[index][2],
-                                "description": menu_items1[index][3],
-                                "gluten_free": menu_items1[index][4],
-                                "vegan": menu_items1[index][5],
-                                "veg": menu_items1[index][6],
-                                "spice": menu_items1[index][7],
-                                "price": menu_items1[index][8],
-                                "course": menu_items1[index][9]
-                                };
+                                               "id": menu_items1[index][0],
+                                               "name": menu_items1[index][2],
+                                               "description": menu_items1[index][3],
+                                               "gluten_free": menu_items1[index][4],
+                                               "vegan": menu_items1[index][5],
+                                               "veg": menu_items1[index][6],
+                                               "spice": menu_items1[index][7],
+                                               "price": menu_items1[index][8],
+                                               "course": menu_items1[index][9]
+                                               };
 
             $scope.menu_items.push(item);
 
             $http.get('/api/restaurant/menu/item/'+ menu_items1[index][0] +'/diets').then(function successCallback(response) {
-               item["allergies"] = response.data;
+
+
+               //item["allergies"] = response.data;
+
+
             });
         }
     });
