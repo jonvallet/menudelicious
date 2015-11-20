a0f71b36a4759349a5f3265644f261daf2158e50
VictoriaJames <victoriaamps@gmail.com>
2015-11-20 09:42:28 +0000

    New restaurant front end files

--- model/src/main/resources/db/migration/V1__Create_TODO_table.sql
+++ model/src/main/resources/db/migration/V1__Create_TODO_table.sql
@@ -4,3 +4,32 @@ create table TODO (
     description varchar(255),
     done boolean
 );
+
+create table restaurant (
+    id int identity(1,1),
+    name varchar(255),
+    description varchar(255),
+    username varchar(40),
+    house_name_no varchar(40),
+    street varchar(40),
+    postcode varchar(8)
+);
+
+create table menu_items (
+    id int identity(1,1),
+    name varchar(255),
+    description varchar(255),
+    like_count int,
+    spice int
+);
+
+create table diet (
+    id int identity(1,1),
+    name varchar(255)
+);
+
+create table menu_diet (
+    id int identity(1,1),
+    menu_item_id int,
+    diet_id int
+);
---
+++ web/src/main/webapp/js/restaurant_cont.js
@@ -0,0 +1,32 @@
+var todoApp = angular.module('restaurant', []);
+
+todoApp.controller('TodoCtrl', function ($scope,$http) {
+
+  $scope.restaurants = [];
+
+  $scope.newRestaurant = {};
+
+  $scope.new = function() {
+    var newItem = {
+      "name":$scope.newRestaurant.name,
+      "description":$scope.newRestaurant.description,
+      "username":$scope.newRestaurant.username,
+      "house_name_no":$scope.newRestaurant.house_name_no,
+      "street":$scope.newRestaurant.street,
+      "postcode":$scope.newRestaurant.postcode
+    };
+    saveNew(newItem)
+  };
+
+  $http.get('/api/restaurant').then(function successCallback(response) {
+    $scope.restaurants = response.data
+  });
+
+  function saveNew(restaurant) {
+    $http.post('/api/restaurant', restaurant).then(function successCallback(response) {
+      $scope.restaurants.push(response.data)
+    })
+  }
+
+
+});
